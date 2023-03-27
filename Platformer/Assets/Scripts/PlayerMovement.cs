1a0de16e5f505ffa7c8eb9fa593d3222996d19db
Rudis159 <77154023+Rudis159@users.noreply.github.com>
2023-03-27 20:11:45 +0300

    update player jumping
    add that player could jump from ground only once

--- Platformer/Assets/Scripts/PlayerMovement.cs
+++ Platformer/Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rigidbody2d;
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+    private BoxCollider2D collider;
 
     private float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
+    [SerializeField] private LayerMask jumpableGround;
 
     private enum MovementState { idle, running, jumping, falling }
 
@@ -20,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -29,7 +32,7 @@ public class PlayerMovement : MonoBehaviour
 
         rigidbody2d.velocity = new Vector2(dirX * moveSpeed, rigidbody2d.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, jumpForce);
         }
@@ -56,6 +59,7 @@ public class PlayerMovement : MonoBehaviour
             state = MovementState.idle;
         }
 
+
         if(rigidbody2d.velocity.y > .1f)
         {
             state = MovementState.jumping;
@@ -66,5 +70,10 @@ public class PlayerMovement : MonoBehaviour
         }
 
         animator.SetInteger("state", (int)state);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
